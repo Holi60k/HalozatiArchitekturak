6faef2b0a41820de4460a0fd0990dcf227672504
Holozsnyák Nándor <holi60@gmail.com>
2016-05-01 00:26:58 +0200

    Mukodik :) Refactor kell meg.

--- client.c
+++ client.c
@@ -10,158 +10,185 @@
 
 void initializeSocket(int *);
 void setSocketOpt(int *);
-void printChosenLanguageAndSendToServer(int,int);
-void connectToServer(int *,struct sockaddr *);
+void printChosenLanguageAndSendToServer(char[], int);
+void connectToServer(int *, struct sockaddr *);
 void toLowerCase(char[]);
-void toUpperCase(char[]); 
+void toUpperCase(char[]);
 int parseOrder(char[]);
 int main() {
-  
-  /**
-   * A szerver címét leíró dolgokat beállítjuk.
-   */
-  struct  sockaddr_in serverAddr;
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(2556);
-  //serverAddr.sin_addr.s_addr = INADDR_ANY;
-  inet_aton("127.0.0.1",&serverAddr.sin_addr);
-  memset(&(serverAddr.sin_zero),0,8);
-  int serverFd;
-  
-  // Inicializáljuk a socketet.
-  initializeSocket(&serverFd);
-  // Beállítjuk a socket beállításait
-  setSocketOpt(&serverFd);
-  //Kapcsolódunk a szerverhez.
-  connectToServer(&serverFd,(struct sockaddr *)&serverAddr);
-  
-  char uzenet[255];
-  int choice = 0;
-  int error = 0;
-  char message[1023];
-  char feedback[255];
-  int read = 0;
-  int sorrend = 0;
-    
-    //Nagybetűs üdvözlés
-    memset(uzenet,0,255);
-    recv(serverFd,uzenet,255,0);
-    printf("%s",uzenet);
-    
-    
-    //Kisbetűs üdvözlés
-    memset(uzenet,0,255);
-    recv(serverFd,uzenet,255,0);
-    printf("%s",uzenet);
-    
-    
-    //Nyelvválasztás.
-    memset(uzenet,0,255);
-    recv(serverFd,uzenet,255,0);
-    printf("%s",uzenet);
-    
-    do {
-    //if(error >0)
-      //printf("Valasszon ujra:");
-    scanf("%d",&choice);
-    //error++;
-    } while(choice !=1 && choice !=2);
-    printChosenLanguageAndSendToServer(choice,serverFd);
-    
-    recv(serverFd,uzenet,255,0);
-    sorrend = parseOrder(uzenet);
-    printf("%d vagyok a felszolalasban.\n",sorrend);
-    fflush(stdout);
-    
-  int round = 1;
-  char lenyeg[255];
-  while(1) {  
-    
-    memset(lenyeg,0,255);
-    read = recv(serverFd,lenyeg,255,0);
-    lenyeg[read] = '\0';
-    printf("%s",lenyeg);
-    //printf("Kor:%d \nSorrend:%d\n",round,sorrend);
-    printf("round maradekos sorrend:%d\n",round % sorrend);
-    //Felszolalas kuldese.
-    if(round % sorrend == 0) {
-      memset(message,0,1023);
-      printf("Szolaljon fel kerem:");
-      gets(message);
-      printf("Felszolalas:%s\n",message);
-      send(serverFd,message,1023,0);
-    } else if (round % sorrend == 1) { //vélemény küldése
-      memset(feedback,0,255);
-      printf("Mondjon velemenyt kerem:");
-      gets(feedback);
-      printf("Velemeny:%s\n",feedback);
-      send(serverFd,feedback,255,0);
-    }
-    
-    round++;
-    
-  }
-  
-  close(serverFd);
-  
+
+	/**
+	 * A szerver címét leíró dolgokat beállítjuk.
+	 */
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(2556);
+	//serverAddr.sin_addr.s_addr = INADDR_ANY;
+	inet_aton("127.0.0.1", &serverAddr.sin_addr);
+	memset(&(serverAddr.sin_zero), 0, 8);
+	int serverFd;
+
+	// Inicializáljuk a socketet.
+	initializeSocket(&serverFd);
+	// Beállítjuk a socket beállításait
+	setSocketOpt(&serverFd);
+	//Kapcsolódunk a szerverhez.
+	connectToServer(&serverFd, (struct sockaddr *) &serverAddr);
+
+	char uzenet[255];
+	char choice[255];
+	int error = 0;
+	char message[1023];
+	char feedback[255];
+	int read = 0;
+	int sorrend = 0;
+
+//	//Nagybetűs üdvözlés
+//	memset(uzenet, 0, 255);
+//	recv(serverFd, uzenet, 255, 0);
+//	printf("%s", uzenet);
+//
+//	//Kisbetűs üdvözlés
+//	memset(uzenet, 0, 255);
+//	recv(serverFd, uzenet, 255, 0);
+//	printf("%s", uzenet);
+
+//Nyelvválasztás.
+//	memset(uzenet, 0, 255);
+//	recv(serverFd, uzenet, 255, 0);
+//	printf("%s", uzenet);
+
+//do {
+//if(error >0)
+//printf("Valasszon ujra:");
+	fgets(choice, 255, stdin);
+	//error++;
+	//} while (strcmp(choice,"MAGYAR\n") != 0 || strcmp(choice,"magyar\n") != 0);
+	printChosenLanguageAndSendToServer(choice, serverFd);
+
+	recv(serverFd, uzenet, 255, 0);
+	sorrend = parseOrder(uzenet);
+	printf("%d vagyok a felszolalasban.", sorrend);
+	fflush(stdout);
+
+	int round = 1;
+	char lenyeg[255];
+	memset(lenyeg, 0, 255);
+	read = recv(serverFd, lenyeg, 255, 0);
+	//lenyeg[read] = '\0';
+	//printf("%s", lenyeg);
+
+	printf("Maradekos %d", round % sorrend);
+	char vote[255], anotherClientVote[255];
+	int msgS = 0, feedbS = 0;
+	for (;;) {
+
+		//Felszolalas kuldese.
+		if (sorrend % 2 == 1) {
+			memset(message, 0, 1023);
+			printf("Szolaljon fel kerem:");
+			fflush(stdout);
+			//scanf("%s%*[^\n]",&message);
+			fgets(message, 1023, stdin);
+
+			send(serverFd, message, 1023, 0);
+			memset(feedback, 0, 255);
+			read = recv(serverFd, feedback, 255, 0);
+			printf("Velemeny:%s", feedback);
+			fflush(stdout);
+			msgS = 1;
+
+		} else if (sorrend % 2 == 0) { //vélemény küldése
+
+			memset(message, 0, 1023);
+			read = recv(serverFd, message, 1023, 0);
+			printf("Felszolalas:%s", message);
+			fflush(stdout);
+
+			memset(feedback, 0, 255);
+			printf("Mondjon velemenyt kerem:");
+			fgets(feedback, 255, stdin);
+			send(serverFd, feedback, 255, 0);
+			fflush(stdout);
+			feedbS = 1;
+		}
+
+		if (feedbS == 1 && msgS == 1) {
+			memset(lenyeg, 0, 255);
+			read = recv(serverFd, lenyeg, 255, 0);
+			printf("%s", lenyeg);
+			fgets(vote, 255, stdin);
+			send(serverFd,vote,255,0);
+			recv(serverFd,anotherClientVote,255,0);
+			printf("masik szavazat:%s",anotherClientVote);
+			if((strcmp(vote,"igen\n") == 0 || strcmp(vote,"IGEN\n") == 0) && (strcmp(anotherClientVote,"igen\n") == 0 || strcmp(anotherClientVote,"IGEN\n") == 0) )
+				break;
+			feedbS = 0;
+			msgS = 0;
+		}
+
+		sorrend++;
+
+	}
+
+	close(serverFd);
+
 }
 
 void initializeSocket(int * sock) {
-  
-  if((*sock = socket(AF_INET,SOCK_STREAM,0)) == -1) {
-    perror("Hiba a socket létrehozásakor!");
-    _exit(1);
-  }
+
+	if ((*sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("Hiba a socket létrehozásakor!");
+		_exit(1);
+	}
 }
 
 void setSocketOpt(int * sock) {
-  
-  int yes = 1;
-  if(setsockopt(*sock,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
-      perror("Hiba a socket beállítása közben.");
-      _exit(1);
-  }
+
+	int yes = 1;
+	if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("Hiba a socket beállítása közben.");
+		_exit(1);
+	}
 }
 
-void printChosenLanguageAndSendToServer(int choice, int sock) {
-  char msg[255];
-  memset(msg,0,255);
-  if(choice == 1) {
-    printf("A VALASZOTT NYELVE:MAGYAR.\n");
-    strcpy(msg,"NAGY");
-  } else {
-    printf("a valasztott nyelve:magyar.\n");
-    strcpy(msg,"kicsi");
-  }
-  send(sock,msg,255,0);
+void printChosenLanguageAndSendToServer(char choice[], int sock) {
+	char msg[255];
+	memset(msg, 0, 255);
+	if (strcmp(choice, "MAGYAR\n") == 0) {
+		//printf("A VALASZOTT NYELVE:MAGYAR.");
+		strcpy(msg, "NAGY");
+	} else if (strcmp(choice, "magyar\n") == 0) {
+		//printf("a valasztott nyelve:magyar.");
+		strcpy(msg, "kicsi");
+	}
+	send(sock, msg, 255, 0);
 }
 
-void connectToServer(int * servSock,struct sockaddr * serverAddr){
-  if(connect(*servSock,serverAddr,sizeof(struct sockaddr)) == -1) {
-    perror("Nem sikerült a szervehez csatlakozni");
-    _exit(1);
-  }
+void connectToServer(int * servSock, struct sockaddr * serverAddr) {
+	if (connect(*servSock, serverAddr, sizeof(struct sockaddr)) == -1) {
+		perror("Nem sikerült a szervehez csatlakozni");
+		_exit(1);
+	}
 }
 
 void toLowerCase(char * message) {
-  int i;
-  for(i = 0; i < strlen(message); i++) {
-    message[i] = tolower(message[i]);
-    //printf("%c,",message[i]);
-  }
+	int i;
+	for (i = 0; i < strlen(message); i++) {
+		message[i] = tolower(message[i]);
+	}
 }
 
 void toUpperCase(char message[]) {
-  int i;
-  for(i = 0; i < strlen(message); i++) {
-    message[i] = toupper(message[i]);
-    printf("%c,",message[i]);
-  }
+	int i;
+	for (i = 0; i < strlen(message); i++) {
+		message[i] = toupper(message[i]);
+	}
 }
 
 int parseOrder(char msg[]) {
-  if(strcmp(msg,"1") == 0) 
-    return 1;
-  if(strcmp(msg,"2") == 0) 
-    return 2;
+	if (strcmp(msg, "1") == 0)
+		return 1;
+	if (strcmp(msg, "2") == 0)
+		return 2;
 }
--- server.c
+++ server.c
@@ -9,219 +9,236 @@
 
 void initializeSocket(int *);
 void setSocketOpt(int *);
-void bindSocket(int *, struct sockaddr * );
+void bindSocket(int *, struct sockaddr *);
 void listenOnSocket(int *, int);
 void toLowerCase(char[]);
-void toUpperCase(char[]); 
+void toUpperCase(char[]);
 void setChosenLanguage(char*, int *);
 void sendMessageToSocket(char *, int);
 void translateMessageForClient(int, char[]);
 
 int main() {
- 
-  /**
-   * A szerver címét leíró dolgokat beállítjuk.
-   */
-  struct  sockaddr_in serverAddr;
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(2556);
-  //serverAddr.sin_addr.s_addr = INADDR_ANY;
-  inet_aton("127.0.0.1",&serverAddr.sin_addr);
-  memset(&(serverAddr.sin_zero),0,8);
-  
-  //Szerver fájlleírója
-  int serverFd;
-  
-  initializeSocket(&serverFd);
-  
-  setSocketOpt(&serverFd);
-  
-  bindSocket(&serverFd,(struct sockaddr *)&serverAddr);
-  
-  listenOnSocket(&serverFd,10);
-  
-  printf("TCP Socket készen áll a kliensek fogadására.\n");
-  fflush(stdout);
-  
-  //Két kliens fáljlleírója, sockaddr_in, illetve a válaszott nyelv.
-  int firstClient;
-  struct sockaddr_in firstClientAddr;
-  int firstLanguage;
-  char firstMessage[1023];
-  char firstFeedback[255];
-  
-  int secondClient;
-  struct sockaddr_in secondClientAddr;
-  int secondLanguage;
-  char secondMessage[1023];
-  char secondFeedback[255];
-  
-  char  messageFromClient[255];
-  char serverMessage[255];
-  strcpy(serverMessage, "Udvozlom a konferencian, kerem valasszon nyelvet.\n");
-  char * choices ="1. MAGYAR\n2. magyar\n";
-  
-  unsigned sin_size = sizeof(struct sockaddr_in);
-  
-  //Várakozunk az első kliens kapcsolódására.
-  firstClient = accept(serverFd, (struct sockaddr *)&firstClientAddr, &sin_size);
-  printf("Első résztvevő csatlakozott.\n");
-  
-  fflush(stdout);
-  
-  //Üdvözlő szövegek elküldése.
-  toUpperCase(serverMessage);
-  send(firstClient,serverMessage,255,0);
-  
-  toLowerCase(serverMessage);
-  send(firstClient,serverMessage,255,0);
-  
-  send(firstClient,choices,strlen(choices),0);
-  //Választott nyelv beállítása.
-  recv(firstClient,messageFromClient,255,0);
-  setChosenLanguage(messageFromClient,&firstLanguage);
-  
-  strcpy(serverMessage,"1");
-  send(firstClient,serverMessage,255,0);
-  
-  //Várakozunk a második kliens csatlakozására.
-  memset(serverMessage,0,255);
-  secondClient = accept(serverFd, (struct sockaddr *)&secondClientAddr, &sin_size);
-  printf("Második résztvevő csatlakozott.\n");
-  strcpy(serverMessage, "Udvozlom a konferencian, kerem valasszon nyelvet.\n");
-  
-  //Üdvözlő szövegek elküldése.
-  toUpperCase(serverMessage);
-  send(secondClient,serverMessage,255,0);
-  
-  toLowerCase(serverMessage);
-  send(secondClient,serverMessage,255,0);
-  
-  send(secondClient,choices,strlen(choices),0);
-  
-  recv(secondClient,messageFromClient,255,0);
-  setChosenLanguage(messageFromClient,&secondLanguage);
-  
-  strcpy(serverMessage,"2");
-  send(secondClient,serverMessage,255,0);
-  
-  //memset(serverMessage,0,255);
-  strcpy(serverMessage,"A második resztvevo is csatlakozott.\nKezodhet a konferencia.\nOn az elso felszolalo.\nKerem mondja el a javaslatat:");
-  translateMessageForClient(firstLanguage,serverMessage);
-  sendMessageToSocket(serverMessage,firstClient);
-  
-  strcpy(serverMessage,"Az elso resztvevo megfogalmazza a felszolalasat.\n");
-  translateMessageForClient(secondLanguage,serverMessage);
-  sendMessageToSocket(serverMessage,secondClient);
-  
-  memset(firstMessage,0,1023);
-  memset(secondMessage,0,1023);
-  memset(firstFeedback,0,255);
-  memset(secondFeedback,0,255);
-  int received = 0;
-  while(1) {
-    
-    
-    
-    //Első felszólal 
-    received = recv(firstClient,firstMessage,1023,0);
-    firstMessage[received] = '\0';
-    printf("Elso kliens felszolalasa:%s\n",firstMessage);
-    fflush(stdout);
-    translateMessageForClient(secondLanguage,firstMessage);
-    send(secondClient,firstMessage,1023,0);
-    
-    //második reagál
-    recv(secondClient,secondFeedback,255,0);
-    printf("Masodik kliens velemenye:%s\n",secondFeedback);
-    translateMessageForClient(firstLanguage,secondFeedback);
-    send(firstClient,secondFeedback,255,0);
-    
-    
-    //Második felszólal
-    recv(secondClient,secondMessage,1023,0);
-    printf("Masodik kliens felszolalasa:%s\n",secondMessage);
-    translateMessageForClient(firstLanguage,secondMessage);
-    send(firstClient,secondMessage,1023,0);
-    
-    //első reagál
-    recv(firstClient,firstFeedback,255,0);
-    printf("Elso kliens velemenye:%s\n",firstFeedback);
-    translateMessageForClient(secondLanguage,firstFeedback);
-    send(secondClient,firstFeedback,255,0);
-       
-  }
-  
-  close(secondClient);
-  close(firstClient);
-  close(serverFd);
-  
+
+	/**
+	 * A szerver címét leíró dolgokat beállítjuk.
+	 */
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(2556);
+	//serverAddr.sin_addr.s_addr = INADDR_ANY;
+	inet_aton("127.0.0.1", &serverAddr.sin_addr);
+	memset(&(serverAddr.sin_zero), 0, 8);
+
+	//Szerver fájlleírója
+	int serverFd;
+
+	initializeSocket(&serverFd);
+
+	setSocketOpt(&serverFd);
+
+	bindSocket(&serverFd, (struct sockaddr *) &serverAddr);
+
+	listenOnSocket(&serverFd, 10);
+
+	printf("TCP Socket készen áll a kliensek fogadására.\n");
+	fflush(stdout);
+
+	//Két kliens fáljlleírója, sockaddr_in, illetve a válaszott nyelv.
+	int firstClient;
+	struct sockaddr_in firstClientAddr;
+	int firstLanguage;
+	char firstMessage[1023];
+	char firstFeedback[255];
+	char firstVote[255];
+
+	int secondClient;
+	struct sockaddr_in secondClientAddr;
+	int secondLanguage;
+	char secondMessage[1023];
+	char secondFeedback[255];
+	char secondVote[255];
+
+	char messageFromClient[255];
+	char serverMessage[255];
+	strcpy(serverMessage,
+			"Udvozlom a konferencian, kerem valasszon nyelvet.");
+	char choices[255] = "1. MAGYAR - 2. magyar";
+
+	unsigned sin_size = sizeof(struct sockaddr_in);
+
+	//Várakozunk az első kliens kapcsolódására.
+	firstClient = accept(serverFd, (struct sockaddr *) &firstClientAddr,
+			&sin_size);
+	printf("Első résztvevő csatlakozott.");
+
+	fflush(stdout);
+
+	//Üdvözlő szövegek elküldése.
+//	toUpperCase(serverMessage);
+//	send(firstClient, serverMessage, 255, 0);
+//
+//	toLowerCase(serverMessage);
+//	send(firstClient, serverMessage, 255, 0);
+
+	//send(firstClient, choices, 255, 0);
+	//Választott nyelv beállítása.
+	recv(firstClient, messageFromClient, 255, 0);
+	setChosenLanguage(messageFromClient, &firstLanguage);
+
+	strcpy(serverMessage, "1");
+	send(firstClient, serverMessage, 255, 0);
+
+	//Várakozunk a második kliens csatlakozására.
+	memset(serverMessage, 0, 255);
+	secondClient = accept(serverFd, (struct sockaddr *) &secondClientAddr,
+			&sin_size);
+	printf("Második résztvevő csatlakozott.");
+//	strcpy(serverMessage,
+//			"Udvozlom a konferencian, kerem valasszon nyelvet.");
+//
+//	//Üdvözlő szövegek elküldése.
+//	toUpperCase(serverMessage);
+//	send(secondClient, serverMessage, 255, 0);
+//
+//	toLowerCase(serverMessage);
+//	send(secondClient, serverMessage, 255, 0);
+
+	//send(secondClient, choices, 255, 0);
+
+	recv(secondClient, messageFromClient, 255, 0);
+	setChosenLanguage(messageFromClient, &secondLanguage);
+
+	strcpy(serverMessage, "2");
+	send(secondClient, serverMessage, 255, 0);
+
+	memset(serverMessage,0,255);
+	//strcpy(serverMessage,"A második resztvevo is csatlakozott.\nKezodhet a konferencia.\nOn az elso felszolalo.\nKerem mondja el a javaslatat:");
+
+	strcpy(serverMessage,"Start!");
+	translateMessageForClient(firstLanguage,serverMessage);
+	send(firstClient, serverMessage, 255, 0);
+	translateMessageForClient(secondLanguage,serverMessage);
+	send(secondClient, serverMessage, 255, 0);
+
+//	memset(firstMessage, 0, 1023);
+//	memset(secondMessage, 0, 1023);
+//	memset(firstFeedback, 0, 255);
+//	memset(secondFeedback, 0, 255);
+	int received = 0;
+	while (1) {
+
+		//Első felszólal
+		received = recv(firstClient, firstMessage, 1023, 0);
+		//firstMessage[received] = '\0';
+		//printf("Elso kliens felszolalasa:%s\n", firstMessage);
+		translateMessageForClient(secondLanguage, firstMessage);
+		send(secondClient, firstMessage, 1023, 0);
+
+		//második reagál
+		recv(secondClient, secondFeedback, 255, 0);
+		//printf("Masodik kliens velemenye:%s\n", secondFeedback);
+		translateMessageForClient(firstLanguage, secondFeedback);
+		send(firstClient, secondFeedback, 255, 0);
+
+		//Második felszólal
+		recv(secondClient, secondMessage, 1023, 0);
+		//printf("Masodik kliens felszolalasa:%s\n", secondMessage);
+		translateMessageForClient(firstLanguage, secondMessage);
+		send(firstClient, secondMessage, 1023, 0);
+
+		//első reagál
+		recv(firstClient, firstFeedback, 255, 0);
+		//printf("Elso kliens velemenye:%s\n", firstFeedback);
+		translateMessageForClient(secondLanguage, firstFeedback);
+		send(secondClient, firstFeedback, 255, 0);
+
+		strcpy(serverMessage,"Szavazas kovetkezik, kerem Igennel,Nemmel vagy tartozkodommal valaszoljanak.");
+		translateMessageForClient(firstLanguage,serverMessage);
+		send(firstClient,serverMessage,255,0);
+
+		translateMessageForClient(secondLanguage,serverMessage);
+		send(secondClient,serverMessage,255,0);
+
+		recv(firstClient,firstVote,255,0);
+		recv(secondClient,secondVote,255,0);
+
+		send(secondClient,firstVote,255,0);
+		send(firstClient,secondVote,255,0);
+
+		if((strcmp(firstVote,"igen\n") == 0 || strcmp(firstVote,"IGEN\n") == 0) && (strcmp(secondVote,"igen\n") == 0 || strcmp(secondVote,"IGEN\n") == 0) )
+				break;
+
+	}
+
+	close(secondClient);
+	close(firstClient);
+	close(serverFd);
+
 }
 
-
 void initializeSocket(int * sock) {
-  
-  if((*sock = socket(AF_INET,SOCK_STREAM,0)) == -1) {
-    perror("Hiba a socket létrehozásakor");
-    _exit(1);
-  }
+
+	if ((*sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("Hiba a socket létrehozásakor");
+		_exit(1);
+	}
 }
 
 void setSocketOpt(int * sock) {
-  
-  int yes = 1;
-  if(setsockopt(*sock,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
-      perror("Hiba a socket beállítása közben");
-      _exit(1);
-  }
+
+	int yes = 1;
+	if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("Hiba a socket beállítása közben");
+		_exit(1);
+	}
 }
 
 void bindSocket(int * sock, struct sockaddr * serverAddr) {
-  
-  if( bind(*sock,serverAddr,sizeof(struct sockaddr)) == -1) {
-    perror("Hiba a socket bind-elése közben");
-    _exit(1);
-  }
+
+	if (bind(*sock, serverAddr, sizeof(struct sockaddr)) == -1) {
+		perror("Hiba a socket bind-elése közben");
+		_exit(1);
+	}
 }
 
 void listenOnSocket(int * sock, int maxClients) {
-  if(listen(*sock,maxClients) == -1) {
-    perror("Hiba a listen alkalmazása közben");
-    _exit(1);
-  }
+	if (listen(*sock, maxClients) == -1) {
+		perror("Hiba a listen alkalmazása közben");
+		_exit(1);
+	}
 }
 
 void toLowerCase(char * message) {
-  int i;
-  for(i = 0; i < strlen(message); i++) {
-    message[i] = tolower(message[i]);
-    //printf("%c,",message[i]);
-  }
+	int i;
+	for (i = 0; i < strlen(message); i++) {
+		message[i] = tolower(message[i]);
+		//printf("%c,",message[i]);
+	}
 }
 
 void toUpperCase(char message[]) {
-  int i;
-  for(i = 0; i < strlen(message); i++) {
-    message[i] = toupper(message[i]);
-  }
+	int i;
+	for (i = 0; i < strlen(message); i++) {
+		message[i] = toupper(message[i]);
+	}
 }
 
 void setChosenLanguage(char* choice, int * language) {
-  if(strcmp(choice,"kicsi") == 0) {
-    *language = 2;
-  } else if(strcmp(choice,"NAGY") == 0) {
-    *language = 1;
-  }
+	if (strcmp(choice, "kicsi") == 0) {
+		*language = 2;
+	} else if (strcmp(choice, "NAGY") == 0) {
+		*language = 1;
+	}
 }
 
 void sendMessageToSocket(char msg[], int sock) {
-   send(sock,msg,255,0);
+	send(sock, msg, 255, 0);
 }
 
 void translateMessageForClient(int language, char msg[]) {
-  if(language == 1) {
-    toUpperCase(msg);
-  } else if (language == 2) {
-    toLowerCase(msg);
-  }
+	if (language == 1) {
+		toUpperCase(msg);
+	} else if (language == 2) {
+		toLowerCase(msg);
+	}
 }
